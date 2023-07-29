d414921858ffaf1eb8c22c55c1fd8123747c1c97
Ivan Tetkić <52411011+ivantetkic@users.noreply.github.com>
2023-07-29 20:08:59 +0200

    OC-11 Commit test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hocemo li da ocenimo nesto?
+         Hocemo li da ocenimo nesto? Commit test!
         </p>
       </header>
     </div>
